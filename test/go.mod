9860f3639f0e44a784a1d96c804c08ae2aada0ce
Fabian Breckle <fabian.breckle@breuninger.de>
2023-08-08 00:39:59 +0200

    wip: Add test file

--- test/go.mod
+++ test/go.mod
@@ -5,3 +5,5 @@ go 1.19
 replace github.com/fbreckle/go-netbox/netbox/netbox => ../netbox
 
 require github.com/fbreckle/go-netbox/netbox/netbox v0.0.0-00010101000000-000000000000
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
--- test/test.go
+++ test/test.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	openapiclient "github.com/fbreckle/go-netbox/netbox/netbox"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+	configuration := openapiclient.NewConfiguration()
+	// configuration.Scheme = "http"
+	// configuration.Host = "localhost:8001"
+	configuration.Servers[0].URL = "http://localhost:8001"
+	spew.Dump(configuration)
+	ctx := context.Background()
+
+	apiKey := openapiclient.APIKey{
+		Key: "0123456789abcdef0123456789abcdef01234567", Prefix: "Token",
+	}
+
+	ctx2 := context.WithValue(ctx, openapiclient.ContextAPIKeys, map[string]openapiclient.APIKey{"tokenAuth": apiKey})
+	apiClient := openapiclient.NewAPIClient(configuration)
+	resp, _, err := apiClient.DcimApi.DcimSitesList(ctx2).Execute()
+	if err != nil {
+		fmt.Printf("Error when calling `DcimApi.DcimSitesList``: %v\n", err)
+		// fmt.Printf("Full HTTP response: %v\n", r)
+	}
+	// response from `DcimSitesList`: PaginatedSiteList
+	fmt.Printf("Response from `DcimApi.DcimSitesList`: %v\n", resp)
 }
